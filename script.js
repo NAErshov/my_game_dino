a808251b2164fcfa07c93118a434be1b4dcd4080
NAErshov <nikolay.ershov.a@gmail.com>
2022-06-04 15:19:28 +0300

    04.06.2022(H)

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ document.addEventListener("keydown", function(event){
 });
 
 function jump () {
-    dino.classList.add("jump")
+    if (dino.classList !="jump") {
+        dino.classList.add("jump")
+    }
+    setTimeout (function(){
+        dino.classList.remove("jump")
+    }, 300)
 }
+let isAlive = setInterval (function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 150) {
+        alert("GAME OVER!!!")
+    }
+}, 10)
